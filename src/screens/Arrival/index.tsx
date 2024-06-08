5695e3494c71a85d710a8719e28bb70a9868a71a
Gabriel Castro <gabrielcssg@gmail.com>
2024-06-08 15:01:52 -0300

    feat: arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useRoute } from '@react-navigation/native';
 
 import * as Styles from './styles';
+import { Header } from '../../components/Header';
+import { Button } from '../../components/Button';
 
 type RouteParamsProps = {
   id: string;
@@ -12,7 +14,24 @@ export function Arrival() {
 
   const { id } = route.params as RouteParamsProps;
 
-  console.log('id >', id);
+  return (
+    <Styles.Container>
+      <Header title="Chegada" />
+      <Styles.Content>
+        <Styles.Label>Placa do veículo</Styles.Label>
+        <Styles.LicensePlate>XXX0000</Styles.LicensePlate>
+        <Styles.Label>Finalidade</Styles.Label>
+        <Styles.Description>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo dicta
+          debitis quam consequuntur dolores amet reiciendis commodi dolor
+          eveniet! Illum autem libero nesciunt repudiandae molestias placeat cum
+          similique in blanditiis?
+        </Styles.Description>
 
-  return <Styles.Container></Styles.Container>;
+        <Styles.Footer>
+          <Button title="Registrar Chegada" />
+        </Styles.Footer>
+      </Styles.Content>
+    </Styles.Container>
+  );
 }
