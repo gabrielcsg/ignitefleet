d1aa260483330569e8e3b267f2295a499553c11c
Gabriel Castro <gabrielcssg@gmail.com>
2024-07-03 20:02:44 -0300

    feat: showing map and coordinates in arrival screen

--- src/screens/Arrival/index.tsx
+++ src/screens/Arrival/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Alert } from 'react-native';
+import { LatLng } from 'react-native-maps';
 import { X } from 'phosphor-react-native';
 import { useNavigation, useRoute } from '@react-navigation/native';
 import { BSON } from 'realm';
@@ -9,10 +10,13 @@ import * as Styles from './styles';
 import { Header } from '../../components/Header';
 import { Button } from '../../components/Button';
 import { ButtonIcon } from '../../components/ButtonIcon';
+
 import { useObject, useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
+import { getStorageLocations } from '../../libs/asyncStorage/locationStorage';
 import { getLastSyncTimestamp } from '../../libs/asyncStorage/syncStorage';
 import { stopLocationTask } from '../../tasks/backgroundLocationTask';
+import { Map } from '../../components/Map';
 
 type RouteParamsProps = {
   id: string;
@@ -22,6 +26,7 @@ export function Arrival() {
   const route = useRoute();
   const { goBack } = useNavigation();
   const [dataNotSynced, setDataNotSynced] = useState(false);
+  const [coordinates, setCoordinates] = useState<LatLng[]>([]);
 
   const { id } = route.params as RouteParamsProps;
 
@@ -30,10 +35,12 @@ export function Arrival() {
 
   const title = historic?.status === 'departure' ? 'Chegada' : 'Detalhes';
 
-  function removeVehicleUsage() {
+  async function removeVehicleUsage() {
     realm.write(() => {
       realm.delete(historic);
     });
+
+    await stopLocationTask();
 
     goBack();
   }
@@ -54,13 +61,11 @@ export function Arrival() {
         );
       }
 
-      await stopLocationTask();
-
       realm.write(() => {
         historic.status = 'arrival';
         historic.updated_at = new Date();
       });
-
+      await stopLocationTask();
       Alert.alert('Chegada', 'Chegada registrada com sucesso!');
       goBack();
     } catch (error) {
@@ -68,15 +73,25 @@ export function Arrival() {
     }
   }
 
+  async function getLocationsInfo() {
+    const lastSync = await getLastSyncTimestamp();
+    const updatedAt = historic!.updated_at.getTime();
+    setDataNotSynced(updatedAt > lastSync);
+
+    const locationsStorage = await getStorageLocations();
+    setCoordinates(locationsStorage);
+  }
+
   useEffect(() => {
-    getLastSyncTimestamp().then((lastSync) =>
-      setDataNotSynced(historic!.updated_at.getTime() > lastSync)
-    );
-  }, []);
+    getLocationsInfo();
+  }, [historic]);
 
   return (
     <Styles.Container>
       <Header title={title} />
+
+      {coordinates.length > 0 && <Map coordinates={coordinates} />}
+
       <Styles.Content>
         <Styles.Label>Placa do veículo</Styles.Label>
         <Styles.LicensePlate>{historic?.license_plate}</Styles.LicensePlate>
